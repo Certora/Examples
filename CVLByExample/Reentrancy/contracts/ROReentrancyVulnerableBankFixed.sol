d1a1620888f75bc785b712074543d12c33126b77
sitvanit <sitvanit@certora.com>
2023-11-07 12:30:37 +0200

    Completed reentrancy documentation. added roreentrancy to reentrancy.

---
+++ CVLByExample/Reentrancy/contracts/ROReentrancyVulnerableBankFixed.sol
@@ -0,0 +1,41 @@
+abstract contract ReentrancyGuard {
+    bool internal locked;
+
+    modifier noReentrant() {
+        require(!locked, "No re-entrancy");
+        locked = true;
+        _;
+        locked = false;
+    }
+}
+
+
+contract ROReentrancyVulnerableBankFixed is ReentrancyGuard {
+    mapping (address => uint256) private userBalances;
+
+    function withdrawAll() external noReentrant{
+        uint256 balance = getUserBalance(msg.sender);
+        require(balance > 0, "Insufficient balance");
+        userBalances[msg.sender] = 0;
+        (bool success, ) = msg.sender.call{value: balance}("");
+        require(success, "Failed to send Ether");
+
+    }
+
+    function withdraw(uint256 amount) external noReentrant {
+        uint256 balance = getUserBalance(msg.sender);
+        require(balance >= amount, "Insufficient balance");
+        userBalances[msg.sender] -= amount;
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "Failed to send Ether");
+
+    }
+
+    function getBalance() external view returns (uint256){
+        return address(this).balance;
+    }
+
+    function getUserBalance(address _user) public view returns (uint256) {
+        return userBalances[_user];
+    }
+}
