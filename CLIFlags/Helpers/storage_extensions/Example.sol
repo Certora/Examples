256a0906f3e9bcca2456892b740902904e2dc352
liav-certora <liav@certora.com>
2025-05-29 12:01:04 +0300

    cr

--- CLIFlags/Helpers/storage_extensions/Example.sol
+++ CLIFlags/Helpers/storage_extensions/Example.sol
@@ -1,19 +1,19 @@
 contract Example {
     uint existing;
 
-	struct Beep {
+    struct Beep {
         uint mask;
-		uint[256] words;
+        uint[256] words;
         mapping(uint => uint) map;
-	}
+    }
 
-	struct Book {
-		Beep field1;
-		Beep field2;
-	}
+    struct Book {
+        Beep field1;
+        Beep field2;
+    }
 
     function getBook1() internal returns (Book storage) {
-		Book storage b;
+        Book storage b;
         assembly ("memory-safe") {
             b.slot := 0x8580001204012435
         }
@@ -21,23 +21,25 @@ contract Example {
     }
 
     function getBook2() internal returns (Book storage) {
-		Book storage b;
+        Book storage b;
         assembly ("memory-safe") {
             b.slot := 0x1234123412341234
         }
         return b;
     }
-	function doThing(uint256 x, bool which) external returns (uint256) {
-		Book storage b = getBook1();
-		Beep storage theBeep = which ? b.field1 : b.field2;
+    function doThing(uint256 x, bool which) external returns (uint256) {
+        Book storage b = getBook1();
+        Beep storage theBeep = which ? b.field1 : b.field2;
         uint8 pos = uint8(x & 255);
+        theBeep.map[pos] = x;
         return theBeep.words[pos];
-	}
+    }
 
-	function doOtherThing(uint256 x, bool which) external returns (uint256) {
-		Book storage b = getBook2();
-		Beep storage theBeep = which ? b.field1 : b.field2;
+    function doOtherThing(uint256 x, bool which) external returns (uint256) {
+        Book storage b = getBook2();
+        Beep storage theBeep = which ? b.field1 : b.field2;
         uint8 pos = uint8(x & 255);
+        theBeep.map[pos] = x;
         return theBeep.words[pos];
-	}
+    }
 }
